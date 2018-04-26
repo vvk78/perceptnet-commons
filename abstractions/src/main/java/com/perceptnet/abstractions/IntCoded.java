3fbbccfdf7305c6d83760f9c02e8c9fa7272b416
vkorovkin <Vladimir.Korovkin@paragon-software.com>
2018-04-26 16:50:01 +0300

    IntCoded added

---
+++ abstractions/src/main/java/com/perceptnet/abstractions/IntCoded.java
@@ -0,0 +1,10 @@
+package com.perceptnet.abstractions;
+
+/**
+ * Special interface for int coded to save expenses on Boxing\Unboxing
+ *
+ * created by vkorovkin on 26.04.2018
+ */
+public interface IntCoded {
+    int getCode();
+}
---
+++ tests/src/main/java/com/perceptnet/commons/tests/TestHelper.java
@@ -0,0 +1,8 @@
+package com.perceptnet.commons.tests;
+
+/**
+ * created by vkorovkin on 26.04.2018
+ */
+public class TestHelper {
+
+}
