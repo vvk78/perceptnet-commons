a0c83a8fbce57b9bac7ab632250f7b99eafa31d7
vkorovkin <Vladimir.Korovkin@paragon-software.com>
2018-06-19 16:38:08 +0300

    Lightweight pseudo dependency injection

--- commons-api/src/main/java/com/perceptnet/api/SimplisticServiceProvider.java
+++ commons-api/src/main/java/com/perceptnet/api/SimplisticServiceProvider.java
@@ -1,12 +1,18 @@
 package com.perceptnet.api;
 
+import com.perceptnet.commons.utils.ClassUtils;
+
 import java.util.concurrent.ConcurrentHashMap;
+import static com.perceptnet.api.SimplisticServiceProvider.ServicePathPair.*;
 
 /**
  * created by vkorovkin (vkorovkin@gmail.com) on 17.06.2018
  */
 class SimplisticServiceProvider implements ServiceProvider {
     private final ConcurrentHashMap<String, Object> knownImplementationsMap = new ConcurrentHashMap<String, Object>();
+
+    SimplisticServiceProvider() {
+    }
 
     @Override
     public <S> S getService(Class<S> serviceClass, String servicePath) {
@@ -18,7 +24,59 @@ class SimplisticServiceProvider implements ServiceProvider {
     }
 
     private String serviceKey(Class serviceClass, String servicePath) {
-        return serviceClass + " " + servicePath;
+        return serviceClass.getName() + " " + servicePath;
+    }
+
+    private void registerKnownDefaultMappingsWhenAvailable() {
+        proble("com.perceptnet.commons.json.JsonService", paths(null, "json"), spp(ItemsLoadService.class), spp(ItemsSaveService.class));
+    }
+
+    private void proble(String className, String[] commonPaths, ServicePathPair ... servicesAndPaths) {
+        Object impl = ClassUtils.createSafely(className);
+        if (impl == null) {
+            return;
+        }
+        for (ServicePathPair spp : servicesAndPaths) {
+            if (spp.service.isAssignableFrom(impl.getClass())) {
+                if (commonPaths != null) {
+                    for (String commonPath : commonPaths) {
+                        registerImpl(spp.service, commonPath, impl);
+                    }
+                }
+                if (spp.paths != null) {
+                    for (String path : spp.paths) {
+                        registerImpl(spp.service, path, impl);
+                    }
+                }
+            }
+        }
+    }
+
+    private String[] paths(String ... paths) {
+        return paths;
+    }
+
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    //                                               I N N E R    C L A S S E S
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    static final class ServicePathPair {
+        Class<?> service;
+        String[] paths;
+
+        private ServicePathPair(Class service, String ... paths) {
+            this.service = service;
+            this.paths = paths;
+        }
+
+        static ServicePathPair spp(Class service, String ... paths) {
+            return new ServicePathPair(service, paths);
+        }
+
+        static ServicePathPair spp(Class service) {
+            return new ServicePathPair(service, null);
+        }
     }
 
 }
