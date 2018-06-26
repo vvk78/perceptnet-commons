4ea5e29ffb904ac3619b44da78a01c288e9681b8
vkorovkin <vkorovkin@gmail.com>
2018-06-26 23:59:56 +0300

    restproxy module added

---
+++ restproxy/api/src/main/java/com/perceptnet/commons/restproxy/MethodCallResult.java
@@ -0,0 +1,9 @@
+package com.perceptnet.commons.restproxy;
+
+/**
+ * created by vkorovkin (vkorovkin@gmail.com) on 26.06.2018
+ */
+public class MethodCallResult {
+    private Object result;
+    private Object error;
+}
