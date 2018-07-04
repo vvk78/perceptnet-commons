e12499aeb3920de0e3d63ae08f7909ba5e4dacdc
vkorovkin <vkorovkin@gmail.com>
2018-07-04 10:19:00 +0300

    restproxy (does not compile)

--- restproxy/api/src/main/java/com/perceptnet/commons/restproxy/MethodCallResult.java
+++
@@ -1,9 +0,0 @@
-package com.perceptnet.commons.restproxy;
-
-/**
- * created by vkorovkin (vkorovkin@gmail.com) on 26.06.2018
- */
-public class MethodCallResult {
-    private Object result;
-    private Object error;
-}
---
+++ restproxy/client/src/main/java/com/perceptnet/commons/restproxy/RestProxyInvocationHandler.java
@@ -0,0 +1,7 @@
+package com.perceptnet.commons.restproxy;
+
+/**
+ * created by vkorovkin (vkorovkin@gmail.com) on 30.06.2018
+ */
+public class RestProxyInvocationHandler {
+}
